c506c85128f8a8fe6f7e8f23dfa94e2034e28cd0
kayo duarte cavalcante <135269175+kayocavalcantte@users.noreply.github.com>
2024-11-20 10:11:37 -0300

    Update Carro.java

--- src/Carro.java
+++ src/Carro.java
@@ -1,6 +1,12 @@
-public class Carro extends Automovel{
+public class Carro extends Automovel implements Transportavel{
 
+    private boolean transportavel;
     public Carro(int nome, int modelo, int placa) {
         super(nome, modelo, placa);
+    }
+
+    @Override
+    public boolean ehTransportavel() {
+        return transportavel;
     }
 }
